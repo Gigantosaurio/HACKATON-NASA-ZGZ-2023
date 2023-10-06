447096f59d7a5120b08c867d34c0a56c965429cf
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-10-07 00:20:53 +0200

    update

---
+++ Landing page/js/main.js
@@ -0,0 +1,17 @@
+function enviar() {
+    const divElement = document.getElementById("popup");
+    const UserText = document.getElementById('textUser').value; // Obtiene el valor del input al hacer clic en "Enviar"
+
+    // Crea un nuevo elemento div para el mensaje
+    const mensajeDiv = document.createElement("div");
+    mensajeDiv.classList.add("card1");
+
+    // Agrega el contenido del mensaje al nuevo elemento
+    mensajeDiv.innerHTML = `
+        <p>${UserText}</p>
+        
+    `;
+
+    // Agrega el nuevo mensaje a la lista de mensajes en "popup"
+    divElement.appendChild(mensajeDiv);
+}
