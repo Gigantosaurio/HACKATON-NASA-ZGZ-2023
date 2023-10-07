c020a7b19fe91ba1ccd73611cbdc60bbb0673edb
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2023-10-07 05:14:08 +0200

    me muero

---
+++ Planetario/js/main.js
@@ -0,0 +1,47 @@
+const planet = 'Venus';
+const apiUrl = `https://planets-17f2.onrender.com/planets/getPlanet?name=${planet}`;
+
+
+
+function getApi(url) {
+    fetch(url)
+        .then(res => res.json())
+        .then(jsonDelBack => {
+            console.log(jsonDelBack);
+            showInfo(jsonDelBack);
+        })
+}
+
+const container = document.getElementById("container");
+const descripcion = document.getElementsByClassName("descripcion");
+const nombrePlaneta = document.getElementsByClassName("titulo");
+
+function showInfo(json) {
+    const nuevoDiv = document.createElement('div');
+    container.innerHTML = ``;
+    nuevoDiv.classList.add('container');
+    const nombre = json.name;
+    const distancia = json.distanceFromSun;
+    const descripcion = json.description;
+    const lunas = json.numberOfMoons;
+    const nombreSignificado = json.namesake;
+    
+    nuevoDiv.innerHTML = `
+    <div class="titulo">
+    ${nombre}
+    
+    </div>
+    <div class="descripcion">
+    Distancia del Sol: ${distancia}.<br>
+    <br>
+    Descripcion: ${descripcion}<br>
+    <br>
+    Numero de lunas: ${lunas}.<br>
+    <br>
+    Significado del nombre: ${nombreSignificado}.<br>
+    </div>`;
+    
+    container.appendChild(nuevoDiv);
+}
+
+getApi(apiUrl);
